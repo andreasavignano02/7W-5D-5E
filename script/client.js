a4d3aab7070f72ebbc47731c0668355cfd89d331
andreasavignano02 <112469730+andreasavignano02@users.noreply.github.com>
2022-10-14 16:57:42 +0200

    implementazione di piccole cose

--- script/client.js
+++ script/client.js
@@ -5,7 +5,7 @@
 //     let arraymovies =trasformazionemovie.data
 //     let contenitorepg = document.getElementById('container')
 //     for (let i = 0 ; i < arraymovies.length ; i++){
-//         contenitorepg.innerHTML += `<div class="stili"><img src="${arraymovies[i].imageUrl}" alt="disney"> ${arraymovies[i].name}</div>`
+//         contenitorepg.innerHTML += `<div class="stili"><img class="arrey-item" src="${arraymovies[i].imageUrl}" alt="disney"> ${arraymovies[i].name}</div>`
 //     }
 // }
 
