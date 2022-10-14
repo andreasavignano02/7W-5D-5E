ee20cae2241575260847e524d012349a18cf1895
andreasavignano02 <112469730+andreasavignano02@users.noreply.github.com>
2022-10-14 16:34:30 +0200

    finito

---
+++ script/client.js
@@ -0,0 +1,14 @@
+// async function listadisnaymovie() {
+    
+//     let moviedisney = await fetch ('https://api.disneyapi.dev/character')
+//     let trasformazionemovie = await moviedisney.json()
+//     let arraymovies =trasformazionemovie.data
+//     let contenitorepg = document.getElementById('container')
+//     for (let i = 0 ; i < arraymovies.length ; i++){
+//         contenitorepg.innerHTML += `<div class="stili"><img src="${arraymovies[i].imageUrl}" alt="disney"> ${arraymovies[i].name}</div>`
+//     }
+// }
+
+// window.onload = async() => {
+//     await listadisnaymovie()
+// }
