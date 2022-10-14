3b3076a56463200c736daa5faa080b4b0d3cd506
andreasavignano02 <112469730+andreasavignano02@users.noreply.github.com>
2022-10-14 17:14:28 +0200

    sistemato

--- script/client.js
+++ script/client.js
@@ -1,14 +1,14 @@
-// async function listadisnaymovie() {
+async function listadisnaymovie() {
     
-//     let moviedisney = await fetch ('https://api.disneyapi.dev/character')
-//     let trasformazionemovie = await moviedisney.json()
-//     let arraymovies =trasformazionemovie.data
-//     let contenitorepg = document.getElementById('container')
-//     for (let i = 0 ; i < arraymovies.length ; i++){
-//         contenitorepg.innerHTML += `<div class="stili"><img class="arrey-item" src="${arraymovies[i].imageUrl}" alt="disney"> ${arraymovies[i].name}</div>`
-//     }
-// }
+    let moviedisney = await fetch ('https://api.disneyapi.dev/characters?limit=100&offset=50')
+    let trasformazionemovie = await moviedisney.json()
+    let arraymovies =trasformazionemovie.data
+    let contenitorepg = document.getElementById('container')
+    for (let i = 0 ; i < arraymovies.length ; i++){
+        contenitorepg.innerHTML += `<div class="stili"><img class="arrey-item" src="${arraymovies[i].imageUrl}" alt="disney"> ${arraymovies[i].name}</div>`
+    }
+}
 
-// window.onload = async() => {
-//     await listadisnaymovie()
-// }
+window.onload = async() => {
+    await listadisnaymovie()
+}
